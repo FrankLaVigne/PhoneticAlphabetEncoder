01e4cfaa3073611b769f3534922b0f92d2946e4b
Frank La Vigne <frankla@microsoft.com>
2019-06-19 14:40:36 -0400

    Changed the cmd harness app

--- PhoneticAlphabetEncoder/Program.cs
+++ PhoneticAlphabetEncoder/Program.cs
@@ -17,7 +17,16 @@ namespace PhoneticAlphabetEncoder
             Console.WriteLine(plainText);
             Console.WriteLine(encodedText);
 
+            Console.WriteLine("Try it for yourself");
+
+
+            var input =  Console.ReadLine();
+
+            Console.WriteLine(phoneticEncoder.Encode(plainText));
+
+
             Console.ReadLine();
+            
 
         }
     }
